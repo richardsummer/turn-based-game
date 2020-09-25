89101589893b39e1474dbce16a45ab863c660d15
Richard Summer <richardforrestsummer@gmail.com>
2020-09-24 21:17:05 -0400

    Richard: Created player and enemy class. Created functions that reduce player and enemy health by a random number, then logs to the console.

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+class Player {
+  constructor() {
+    this.health = 100;
+  }
+}
 
+class Enemy {
+  constructor() {
+    this.health = 100;
+  }
+}
+
+let player = new Player;
+let enemy = new Enemy;
+
+function attackEnemy() {
+  const damageValues = [0, 5, 10];
+  const damage = damageValues[Math.floor(Math.random() * damageValues.length)];
+  enemy.health -= damage;
+  console.log("Ememy's Health:" + enemy.health);
+}
+
+function attackPlayer() {
+  const damageValues = [0, 5, 10];
+  const damage = damageValues[Math.floor(Math.random() * damageValues.length)];
+  player.health -= damage;
+  console.log("Player's Health:" + player.health);
+}
+
+attackEnemy();
+attackPlayer();
