91c5f2f3a8901a27582381ed01ea6572dae53772
altruant <noellehamberis@gmail.com>
2020-09-25 11:48:11 -0400

    attack button implemented

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ class Enemy {
   }
 }
 
+const playerClasses = ["Knight", "Elf", "Dwarf"];
 let player = new Player;
 let enemy = new Enemy;
 
@@ -17,15 +18,40 @@ function attackEnemy() {
   const damageValues = [0, 5, 10];
   const damage = damageValues[Math.floor(Math.random() * damageValues.length)];
   enemy.health -= damage;
-  console.log("Ememy's Health:" + enemy.health);
+  console.log("Enemy's Health:" + enemy.health);
+  console.log(`Enemy took ${damage} damage`)
 }
 
 function attackPlayer() {
-  const damageValues = [0, 5, 10];
+  const damageValues = [0,1,1,1,1,1,1,2,3,4,10];
   const damage = damageValues[Math.floor(Math.random() * damageValues.length)];
   player.health -= damage;
-  console.log("Player's Health:" + player.health);
+  console.log("Your Health:" + player.health);
+  console.log(`You took ${damage} damage`)
 }
 
-attackEnemy();
-attackPlayer();
+function isAlive() {
+  if(player.health <= 0) {
+    console.log('You have been defeated!')
+    attackButton.removeEventListener('click', clickAttack)
+  } else if(enemy.health <= 0) {
+    console.log('The enemy has been defeated!')
+    attackButton.removeEventListener('click', clickAttack)
+  }
+  else {
+    return;
+  }
+}
+
+function clickAttack(e) {
+  attackEnemy();
+  attackPlayer();
+  isAlive();
+}
+
+// attackEnemy();
+// attackPlayer();
+
+const attackButton = document.querySelector('.attack');
+
+attackButton.addEventListener('click', clickAttack);
